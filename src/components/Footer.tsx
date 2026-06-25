013149a8497bccacd6f118bb09008d8b35b2fef8
hatfieldq <qhatfield27@gmail.com>
2026-06-25 19:25:58 -0400

    Style Footer, Add links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+
 export default function Footer() {
     return (
-        <footer>
+        <footer className="flex items-center justify-between px-4 py-2 text-14 font-var(--font-orbitron)">
             <p>&copy; 2026 My Portfolio. All rights reserved.</p>
+            <div className="flex items-center gap-4">
+                <a href="https://github.com/hatfieldq" target="_blank" rel="noopener noreferrer" className="hover:text-green-500"> <FaGithub/> </a>
+                <a href="https://linkedin.com/in/quinnten-hatfield" target="_blank" rel="noopener noreferrer" className="hover:text-green-500"> <FaLinkedin/> </a>
+            </div>
         </footer>
     );
 }

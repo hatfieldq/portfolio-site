713a0334bca40d4f2f1f6786f3b43ca3b88a7435
hatfieldq <qhatfield27@gmail.com>
2026-07-29 21:17:39 -0400

    Edit About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,30 +18,33 @@ export default function About() {
             <div>
                 <h1 className="text-2xl font-bold">About Me</h1>
                 <p className="indent-8 mt-4">Hey, I'm Quinnten! I'm a modeling engineer who is interested in creating
-                     physics-based models for real-world applications. This site is a place 
-                     where I share my work and thoughts related to my career and life interests. 
-                     I'll be updating this regularly with content related to my projects and learning, 
-                     including tutorials, interactive demos, and more. Hopefully some of this can 
-                     be helpful to you as well. DISCLAIMER: The information on this site is for educational purposes 
-                     only.</p>
+                     physics-based models for real-world applications. In particular, I am excited about estimation
+                     algorithms for geolocation and tracking, signal processing, scenario-level simulation, and autonomy.
+                     This site is where I will share my work and thoughts related to my career and life interests. 
+                     I am passionate about teaching and sharing knowledge and resources to help set others up for success.
+                     A lot of this content will be directed towards people new to these fields. I'll be updating this regularly with content related to my projects and learning, 
+                     including tutorials, interactive demos, resources, and more.  If you have any comments or questions on the work
+                     I show here, don't hesitate to reach out!
+                </p>
+                <p className="mt-4">  See what I've been building on my{" "}
+                <Link href="/projects" className="text-blue-500 hover:underline">projects page</Link>.
+                </p>
                 <h2 className="text-xl font-semibold mt-4">Interests</h2>
-                <ul>
-                    <li>Signal Processing</li>
-                    <li>Geolocation/Tracking</li>
-                    <li>Learning</li>
+                <ul className="mt-2">
+                    <li className="list-disc pl-2 ml-4">RF Signal Processing</li>
+                    <li className="list-disc pl-2 ml-4">Estimation Techniques for Geolocation</li>
+                    <li className="list-disc pl-2 ml-4">Satellite Systems</li>
+                    <li className="list-disc pl-2 ml-4">Autonomous Systems</li>
                 </ul>
-                <h2 className="text-xl font-semibold mt-4">Recent Projects</h2>
-                <ol>
-                    <li>Project 1</li>
-                    <li>Project 2</li>
-                    <li>Project 3</li>
-                </ol>
+                
+
+
                 <h2 className="text-xl font-semibold mt-4">Contact Information</h2>
-                <ol>
+                <ul className="mt-2">
                     <li>Email: qhatfield27@gmail.com</li>
                     <li>LinkedIn: <a href="https://linkedin.com/in/quinnten-hatfield" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">linkedin.com/in/quinnten-hatfield</a></li>
                     <li>GitHub: <a href="https://github.com/hatfieldq" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">github.com/hatfieldq</a></li>
-                </ol>
+                </ul>
 
             </div>
 
